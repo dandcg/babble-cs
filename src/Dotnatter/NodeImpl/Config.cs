99e71afe38a7d1d154ff3d6131fca30ee295a72f
Daniel Grimes <dan@dcgnet.co.uk>
2018-01-19 14:59:28 +0000

    rough impl of config

--- src/Dotnatter/NodeImpl/Config.cs
+++ src/Dotnatter/NodeImpl/Config.cs
@@ -1,12 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Dotnatter.NodeImpl
 {
-
     public class Config
     {
-        
+        public TimeSpan HeartbeatTimeout { get; set; }
+        public TimeSpan TcpTimeout { get; set; }
+        public int CacheSize { get; set; }
+        public int SyncLimit { get; set; }
+        public string StoreType { get; set; }
+        public string StorePath { get; set; }
+
+        public Config(TimeSpan heartbeatTimeout,
+            TimeSpan tcpTimeout,
+            int cacheSize,
+            int syncLimit,
+            string storeType,
+            string storePath)
+
+        {
+            HeartbeatTimeout = heartbeatTimeout;
+            TcpTimeout = tcpTimeout;
+            CacheSize = cacheSize;
+            SyncLimit = syncLimit;
+            StoreType = storeType;
+            StorePath = storePath;
+        }
+
+        protected Config()
+        {
+        }
+
+        public static Config DefaultConfig()
+        {
+            var storeType = "badger";
+            var storePath = "badger";
+
+            return new Config
+            {
+                HeartbeatTimeout = new TimeSpan(0, 0, 0, 0, 1000),
+                TcpTimeout = new TimeSpan(0, 0, 0, 0, 1000),
+                CacheSize = 500,
+                SyncLimit = 100,
+                StoreType = storeType,
+                StorePath = storePath
+            };
+        }
+
+        public static Config TestConfig()
+        {
+            return new Config
+            {
+                StoreType = "inmem"
+            };
+        }
     }
 }
