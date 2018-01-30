3a431d35e3352cddf75676ca27a609fcf21591f3
Daniel Grimes <dan@dcgnet.co.uk>
2018-01-30 10:18:20 +0000

    Implement control timer

---
+++ src/Dotnatter/NodeImpl/ControlTimer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Nito.AsyncEx;
+
+namespace Dotnatter.NodeImpl
+{
+    public class ControlTimer
+    {
+        private readonly Func<TimeSpan> timerFactory;
+
+        public ControlTimer(Func<TimeSpan> timerFactory)
+        {
+            this.timerFactory = timerFactory;
+            TickCh = new AsyncProducerConsumerQueue<bool>();
+            ResetCh = new AsyncProducerConsumerQueue<bool>();
+            StopCh = new AsyncProducerConsumerQueue<bool>();
+            ShutdownCh = new AsyncProducerConsumerQueue<bool>();
+        }
+
+        public AsyncProducerConsumerQueue<bool> TickCh { get; }
+
+        public AsyncProducerConsumerQueue<bool> ResetCh { get; }
+
+        public AsyncProducerConsumerQueue<bool> StopCh { get; }
+
+        public AsyncProducerConsumerQueue<bool> ShutdownCh { get; }
+
+        public bool Set { get; private set; }
+
+        public static ControlTimer NewRandomControlTimer(TimeSpan baseDuration)
+        {
+            TimeSpan RandomTimeout()
+            {
+                //Todo: For consistency change to System.Cryptography
+                var r = new Random();
+                var value = (long) ((r.NextDouble() * 2.0 - 1.0) * long.MaxValue);
+
+                var minVal = baseDuration;
+                if (minVal.Ticks == 0)
+                {
+                    return TimeSpan.Zero;
+                }
+
+                var extra = value % minVal.Ticks;
+                return TimeSpan.FromTicks(minVal.Ticks + extra);
+            }
+
+            return new ControlTimer(RandomTimeout);
+        }
+
+        public async Task RunAsync(CancellationToken ct)
+        {
+            var timerTask = new Task(async () =>
+            {
+                while (!ct.IsCancellationRequested)
+                {
+                    var dur = timerFactory();
+                    if (dur.Ticks == 0)
+                    {
+                        break;
+                    }
+
+                    await Task.Delay(dur, ct);
+                    await TickCh.EnqueueAsync(true, ct);
+                    Set = false;
+                }
+            });
+            var resetTask = new Task(async () =>
+            {
+                while (!ct.IsCancellationRequested)
+                {
+                    await ResetCh.OutputAvailableAsync(ct);
+                    await ResetCh.DequeueAsync(ct);
+                    Set = true;
+                }
+            });
+            var stopTask = new Task(async () =>
+            {
+                while (!ct.IsCancellationRequested)
+                {
+                    await StopCh.OutputAvailableAsync(ct);
+                    await StopCh.DequeueAsync(ct);
+                    Set = false;
+                }
+            });
+            var shutdownTask = new Task(async () =>
+            {
+                await ShutdownCh.OutputAvailableAsync(ct);
+                await ShutdownCh.DequeueAsync(ct);
+                Set = false;
+            });
+
+            await Task.WhenAny(Task.WhenAll(timerTask, resetTask, stopTask), shutdownTask, Task.Delay(Timeout.Infinite, ct));
+        }
+    }
+}
