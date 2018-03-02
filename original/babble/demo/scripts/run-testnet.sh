e9434054e61d1afa181b44880ee939450d41d511
Daniel Grimes <dan@dcgnet.co.uk>
2018-03-02 11:42:21 +0000

    commit not needed items from original so I can clearly see diffs

--- original/babble/demo/scripts/run-testnet.sh
+++ original/babble/demo/scripts/run-testnet.sh
@@ -14,7 +14,7 @@ docker network create \
 
 for i in $(seq 1 $N)
 do
-    docker create --name=node$i --net=babblenet --ip=172.77.5.$i mosaicnetworks/babble run \
+    docker create --name=node$i --net=babblenet --ip=172.77.5.$i mosaicnetworks/babble:0.2.1 run \
     --cache_size=50000 \
     --tcp_timeout=200 \
     --heartbeat=10 \
@@ -22,15 +22,14 @@ do
     --proxy_addr="172.77.5.$i:1338" \
     --client_addr="172.77.5.$(($N+$i)):1339" \
     --service_addr="172.77.5.$i:80" \
-    --sync_limit=500
+    --sync_limit=500 \
+    --store="inmem"
     docker cp $MPWD/conf/node$i node$i:/.babble
     docker start node$i
 
-    docker run -d --name=client$i --net=babblenet --ip=172.77.5.$(($N+$i)) -it mosaicnetworks/dummy \
+    docker run -d --name=client$i --net=babblenet --ip=172.77.5.$(($N+$i)) -it mosaicnetworks/dummy:0.2.1 \
     --name="client $i" \
     --client_addr="172.77.5.$(($N+$i)):1339" \
     --proxy_addr="172.77.5.$i:1338" \
-    --log_level="info"  
+    --log_level="info" 
 done
-
-docker run --name=watcher --net=babblenet --ip=172.77.5.$(($N+$N+1)) -it mosaicnetworks/watcher
