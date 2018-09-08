fd0692949893702114684bee6c78566fdff3b2b5
dandcg <dan@dcgnet.co.uk>
2018-09-08 16:23:19 +0100

    Add version file

---
+++ src/Babble.Core/Version.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Babble.Core
+{
+    // Todo : This is for alignment with Golang - change to dotnet style soon.
+    public class Version
+    {
+        public const int Maj = 0;
+        public const int Min = 3;
+        public const int Fix = 0;
+
+    }
+}
